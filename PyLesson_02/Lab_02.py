4f2b17c3eaa6f503ae51f76b4e4a1bfc7dc79bbd
JasonBehrend7 <jasonbehrend@gmail.com>
2016-09-06 10:17:13 -0700

    Added lesson 2 files

---
+++ PyLesson_02/Lab_02.py
@@ -0,0 +1,23 @@
+name = "Main Street"
+address = 1234
+city = "San Diego, CA"
+zip = 92130
+
+length = 7
+width = 4
+area = length * width
+length2 = 9
+width2 = 2
+area2 = length2 * width2
+
+w = 4
+h = 3
+l = 5
+a = 2*(w*l+h*l+h*w)
+
+print ("I live at",address, name, city, zip)
+print ("")
+print ("The area of the first rectangle is", area)
+print ("The area of the second rectangle is", area2)
+print ("")
+print ("The surface area of the rectangular prism is", a)
---
+++ PyLesson_02/Pylesson_02.py
@@ -0,0 +1,5 @@
+width = 15
+height = 7
+area = width * height
+
+print ("The area of your rectangle is", area)
